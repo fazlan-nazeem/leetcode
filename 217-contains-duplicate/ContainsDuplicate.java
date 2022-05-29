c1e5303bfca38fa7df281be2c83cd4cebfdc9e2c
Fazlan Nazeem <fazlann@wso2.com>
2022-05-29 13:19:18 +0530

    contains duplicate

--- 217-contains-duplicate/ContainsDuplicate.java
+++ 217-contains-duplicate/ContainsDuplicate.java
@@ -1,2 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> seenElements = new HashSet<>();
+        for(int item: nums) {
+            if(seenElements.contains(item)) {
+                return true;
+            }
+            seenElements.add(item);
+        }
+        return false;
+    }
 }
+
+// time-complexity = O(n)
+// space-complexity = O(n)
