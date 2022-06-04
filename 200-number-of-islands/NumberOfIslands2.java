2cde3efe1ed1cf34ce334f4d28b804a517a75fc6
Fazlan Nazeem <fazlann@wso2.com>
2022-06-04 23:41:56 +0530

    Number of Islands 2

--- 200-number-of-islands/NumberOfIslands2.java
+++ 200-number-of-islands/NumberOfIslands2.java
@@ -8,16 +8,17 @@ public class NumberOfIslands2 {
     int rows;
     int cols;
     boolean[][] visited;
+
     public int numIslands(char[][] grid) {
         this.grid = grid;
         this.rows = grid.length;
         this.cols = grid[0].length;
         visited = new boolean[rows][cols];
         int numOfIslands = 0;
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                if(grid[i][j] == '1' && !visited[i][j]) {
-                    dfs(i,j);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    dfs(i, j);
                     numOfIslands++;
                 }
             }
@@ -44,7 +45,8 @@ public class NumberOfIslands2 {
                 int[] nNode = directions[k];
                 int newRow = row + nNode[0];
                 int newCol = col + nNode[1];
-                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && !visited[newRow][newCol] && grid[newRow][newCol] == '1') {
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && !visited[newRow][newCol] &&
+                        grid[newRow][newCol] == '1') {
                     List<Integer> newNode = new ArrayList<>();
                     newNode.add(newRow);
                     newNode.add(newCol);
